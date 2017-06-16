49e89ace5ede709abb8dfefc5ccbae49c9b71a23
gonghan <desstiony@163.com>
2017-06-16 20:43:25 +0800

    remove old

--- ssh_studmanage/src/com/action/UserLoginAction.java
+++
@@ -1,9 +0,0 @@
-package com.action;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class UserLoginAction extends ActionSupport {
-	public String login(){
-		return LOGIN;
-	}
-}
--- ssh_studmanage/src/com/dao/UserDaoImpl.java
+++
@@ -1,5 +0,0 @@
-package com.dao;
-
-public class UserDaoImpl implements UserDao {
-	
-}
